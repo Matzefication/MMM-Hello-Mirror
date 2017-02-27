3411f524019ec7069e853693d8548bb79f7a7436
Matzefication <Mathias@Kaniut.de>
2017-02-27 13:55:43 +0100

    Update MMM-Hello-Mirror.js

--- MMM-Hello-Mirror.js
+++ MMM-Hello-Mirror.js
@@ -5,11 +5,46 @@
  * MIT Licensed
  */
 
-Module.register("MMM-Hello-Mirror", {
+Module.register("MMM-Hello-Mirror",{
+	// Default module config.
+    	defaults: {
+        	language: "de",
+		text: "Hello World!"
+    	},
+
+	// Called when all modules are loaded an the system is ready to boot up
+	start: function() {
+		this.mySpecialProperty = "So much wow!";
+		Log.log(this.name + ' is started!');
+	},	
+
+	// Load required additional scripts
+	getScripts: function() {
+		return [
+			'//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js',  // annyang! SpeechRecognition
+			'moment.js' // Parse, validate, manipulate, and display dates in JavaScript
+		]
+	},
 	
-  // Default module config.
-	defaults: {
-		language: "de"
-	}
-  
+	// Define additional styles
+	getStyles: function() {
+		return [
+			this.file('style.css')
+		];
+	},
+	
+	// Request translation files
+		getTranslations: function() {
+		    return {
+			    en: "translations/en.json",
+			    de: "translations/de.json"
+		    }
+	},
+	
+    	// Override dom generator.
+    	getDom: function() {
+        	var wrapper = document.createElement("div");
+        	wrapper.innerHTML = this.config.text;
+        	return wrapper;
+    	}	
 });
